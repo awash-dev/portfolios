e1b3556bece4610e9f22cce223e282ba8409cafb
Mohammed Hussen <99973399+Mthacket@users.noreply.github.com>
2024-01-08 13:43:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+
+    // scroll-up button show/hide script
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
+  });
+
+  $(".navbar .menu li a").click(function () {
+    // applying again smooth scroll on menu items click
+    $("html").css("scrollBehavior", "smooth");
+  });
+
+  // toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: [
+      "YouTuber",
+      "Developer",
+      "Ethical hacker",
+      "Designer",
+      "Freelancer",
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  var typed = new Typed(".typing-2", {
+    strings: [
+      "YouTuber",
+      "Developer",
+      "Ethical hacker",
+      "Designer",
+      "Freelancer",
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  // owl carousel script
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
+});
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbxGlXiUz4MoT6sFG8x5AVwuLeEFrshLsnv0Cnh6uhRIHWOTSe2iGCLtB4ogSkc3YN8o2A/exec";
+const form = document.forms["submit-to-google-sheet"];
+
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      });
+      form.reset();
+      alert("Message sent successfully");
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
